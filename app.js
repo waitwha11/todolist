6b7843702fcf0af7b1bd4531d9508e768dc86e0a
waitwha11 <102921946+waitwha11@users.noreply.github.com>
2023-08-25 19:58:03 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+const inputElement = document.getElementById('title')
+const createBtn = document.getElementById('create')
+const listElement = document.getElementById('list')
+
+
+const notes = [
+    {
+        title: 'блабла',
+        completed: false,
+    },
+    {
+        title: 'блаблабла',
+        completed: true,
+    },
+]
+
+function render () {
+    listElement.innerHTML = ''
+    for (let i=0; i<notes.length; i++) {
+        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i))
+    }
+}
+
+render() 
+
+createBtn.onclick = function() {
+    if (inputElement.value.length === 0){
+        return
+    }
+    const newNote = {
+        title: inputElement.value,
+        completed: false,
+    }
+    notes.push(newNote)
+    render()
+inputElement.value = ''
+}
+
+listElement.onclick = function (event) {
+    if(event.target.dataset.index) {
+        const index = Number(event.target.dataset.index)
+        const type = event.target.dataset.type
+
+        if (type === 'toggle') {
+            notes[index].completed = !notes[index].completed 
+        } else if (type === 'remove') {
+            notes.splice(index, 1)
+        }
+        render()
+    }
+}
+
+function getNoteTemplate(i,index) {
+    return `<li
+    class="list-group-item d-flex justify-content-between align-items-center"
+>
+    <span class="${i.completed ? 'text-decoration-line-through' : ''}">${i.title}</span>
+    <span>
+    <span class="btn btn-small btn-${i.completed ? 'warning' : 'success'}" data-index="${index}" data-type="toggle">&check;</span>
+    <span class="btn btn-small btn-danger" data-index="${index}" data-type="remove">&times;</span>
+    </span>
+</li>`
+}
+
